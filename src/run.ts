2af1e4b3d63a44d304f03300f488b56d7cb1d879
梁耀天 <liangyaotian@kollway.com>
2017-08-18 09:40:22 +0800

    commonjs module

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,8 @@
-import run = require('./index')
+import auto = require('./index')
 import path = require('path')
 const root = path.resolve(__dirname, "../")
 
-run({
+auto.run({
     input_sql_file: root + '/flowerws.2017050516.sql',
     output_dir: '/Users/lyaotian/Documents/Web/feathers/test1/src/database/define',
     force_output: true,
